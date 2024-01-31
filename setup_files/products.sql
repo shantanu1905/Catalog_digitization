1a600fc0c25bfd990b19eef67c23707e3a11ea46
shantanu1905 <nimkarshantanu07@gmail.com>
2024-02-01 00:52:13 +0530

    setup files added - products.sql, automation scripts

---
+++ setup_files/products.sql
@@ -0,0 +1,11 @@
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    image_url VARCHAR(255),
+    ean VARCHAR(255),
+    brand VARCHAR(255),
+    category VARCHAR(255),
+    price REAL,
+    description TEXT,
+    embedding VECTOR(2048)
+);
