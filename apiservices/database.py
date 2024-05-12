7478e394f3f5fc0619db954d2a4d788fbc5a13a9
shantanu1905 <nimkarshantanu07@gmail.com>
2024-05-12 12:17:48 +0530

    new

--- apiservices/database.py
+++ apiservices/database.py
@@ -1,13 +1,32 @@
-import sqlalchemy as _sql
-import sqlalchemy.ext.declarative as _declarative
-import sqlalchemy.orm as _orm
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+from .logger import Logger
 
-DATABASE_URL = "sqlite:///./database.db"
+log = Logger()
+logger = log.get_logger(__name__)
+load_dotenv()
+"""Create a connection to the backend default database """
+DATABASE_URL = (
+    "postgresql://{username}:{password}@{host}:{port}/{database_name}".format(
+        username=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        database_name=os.getenv("DB_NAME"),
+    )
+)
 
-engine = _sql.create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
-SessionLocal = _orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(DATABASE_URL)
 
-Base = _declarative.declarative_base()
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 
 
+# Dependency
+def get_db():
+    db = SessionLocal()
+    return db
